47317a12d042d96306dae715f41945698f7813b6
huy <tranquochuy15042003@gmail.com>
2024-09-15 15:01:33 +0700

    w

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Screen_01 from './screens/Screen_01';
+// import Screen_02 from './screens/Screen_02';
+// import Screen_03 from './screens/Screen_03';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Screen_01" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Screen_01" component={Screen_01} />
+      
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ screens/Screen01.js
@@ -0,0 +1,73 @@
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+export default function Screen01({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.style1}>
+        <Text style={{ color: 'green', fontSize: 30, fontWeight: 'bold' }}>
+          Order your favorite!
+        </Text>
+      </View>
+  
+      <View style={styles.style2}>
+        <Image
+          source={require('../Data/Image 95.png')}
+          style={{ width: 150, height: 150, marginLeft: 220 }}
+        />
+        <Image
+           source={require('../Data/Image_96.png')}
+          style={{ width: 150, height: 150, marginLeft: 50 }}
+        />
+        <Image
+           source={require('../Data/Image 97.png')}
+          style={{
+            width: 150,
+            height: 150,
+            marginLeft: 0,
+            marginTop: 50,
+            marginLeft: 220,
+          }}
+        />
+      </View>
+
+      <View style={styles.style3}>
+        <TouchableOpacity
+          style={{
+            backgroundColor: 'green',
+            borderRadius: 40,
+            width: 240,
+            height: 50,
+            marginTop: 50,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          onPress={() => navigation.navigate('Screen_02')}>
+          <Text style={{ color: 'white' }}>Go to Screen 02</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  style1: {
+    alignItems: 'center',
+    marginBottom: 30,
+
+  },
+  style2: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    marginBottom: 20,
+    
+  },
+  style3: {
+    alignItems: 'center',
+  },
+});
